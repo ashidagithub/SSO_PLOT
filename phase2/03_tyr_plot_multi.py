7bce1b08993b32f93b0ec8f57173fbe191f614ee
ashidagithub <1019726435@qq.com>
2019-08-15 17:20:06 +0800

    updated

--- phase2/03_tyr_plot_multi.py
+++ phase2/03_tyr_plot_multi.py
@@ -11,7 +11,14 @@
 
 import matplotlib.pyplot as plt  # 约定俗成的写法plt
 import numpy as np
+import os
 
+def get_path (no):
+    img_path =  os.getcwd() + '\\saved_imgs\\test_try%d.png' % no
+    return img_path
+
+# 两种方法实现
+# https://www.cnblogs.com/xiaopengli/p/8058408.html
 '''
 在一块画布 - Figure上创建多个子plot
 
@@ -19,32 +26,54 @@ import numpy as np
 如果所指定序号的绘图对象已经存在的话，将不创建新的对象，而只是让它成为当前绘图对象。
 
 '''
+img_dpi = 80
 
-'''
+
 # Try1 --------------------------------------------------------
-fig1 = plt.figure(2)
+fig1 = plt.figure()
 plt.subplot(211)
 # subplot(211)把绘图区域等分为2行*1列共两个区域，然
 # 后在区域1（上区域）中创建一个轴对象
 plt.subplot(212)  # 在区域2（下区域）创建一个轴对象
+'''
+保存Figure对象
+
+最后一项操作就是保存，我们绘图的目的是用在其他研究中，
+或者希望可以把研究结果保存下来，并通过 Web 展示，此时需要的操作时save。
+'''
+plt.savefig(get_path(1), dpi=img_dpi)  # 默认像素dpi是80
 plt.show()
 
+
 # Try2 --------------------------------------------------------
-fig1 = plt.figure(3)  # 弹出对话框时的标题，如果显示的形式为弹出对话框的话
-plt.subplot(221)
-plt.subplot(222)
-plt.subplot(212)
+fig2 = plt.figure()  # 弹出对话框时的标题，如果显示的形式为弹出对话框的话
+pst1 = plt.subplot(221)
+pst2 = plt.subplot(222)
+pst3 = plt.subplot(212)
+
 plt.subplots_adjust(left=0.08, right=0.95, wspace=0.25, hspace=0.45)
 # subplots_adjust的操作时类似于网页css格式化中的边距处理，左边距离多少？
 # 右边距离多少？这取决于你需要绘制的大小和各个模块之间的间距
+print(type(pst1))
+
+plt.savefig(get_path(2), dpi=img_dpi)  # 默认像素dpi是80
 plt.show()
 
-'''
 
 # Try3 --------------------------------------------------------
-row = 5
-col = 5
-fig, axes = plt.subplots(row, col)  # 定一个2*2的plot
+row = 2
+col = 2
+fig3 = plt.figure()
+ax1 = fig3.add_axes([0.1,0.1,0.8,0.8])
+ax2 = fig3.add_axes([0.25,0.25,0.4,0.4])
+print(type(ax1))
+
+plt.savefig(get_path(3), dpi=img_dpi)  # 默认像素dpi是80
+plt.show()
+
+# Try4 --------------------------------------------------------
+# 高级用法，直接把子图设置成数组形式
+fig4, axes = plt.subplots(row, col)  # 定一个2*2的plot
 axes[0, 0].set(title='Upper Left')
 axes[0, 1].set(title='Upper Right')
 axes[1, 0].set(title='Lower Left')
@@ -53,5 +82,7 @@ axes[1, 1].set(title='Lower Right')
 # 通过Axes的flat属性进行遍历
 for ax in axes.flat:   # 展平处理
     #     xticks和yticks设置为空置
-    ax.set(xticks=[], yticks=[])
+    ax.set(xticks=[], yticks=[])  # 不要 x 和 y 轴的数值
+
+plt.savefig(get_path(4), dpi=img_dpi)  # 默认像素dpi是80
 plt.show()
